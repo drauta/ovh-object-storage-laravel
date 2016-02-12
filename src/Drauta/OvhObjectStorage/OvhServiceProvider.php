99c07f246b77549ea0c6e2e31d35b08599a9141d
Oscar Delgado <hoosksky@gmail.com>
2016-02-12 10:45:50 +0100

    Update OvhServiceProvider.php

--- src/Drauta/OvhObjectStorage/OvhServiceProvider.php
+++ src/Drauta/OvhObjectStorage/OvhServiceProvider.php
@@ -8,43 +8,39 @@ use Illuminate\Support\ServiceProvider;
 class OvhServiceProvider extends ServiceProvider {
 
 
-	public function boot()
+  public function boot()
   {
-        Storage::extend('ovh', function($app, $config)
-        {
-            $client = new OvhClient($config);   
-            return $client;
-        });
-    }
+    Storage::extend('ovh', function($app, $config)
+    {
+      $client = new OvhClient($config);   
+      return $client;
+    });
+  }
+  /**
+  * Indicates if loading of the provider is deferred.
+  *
+  * @var bool
+  */
+  protected $defer = false;
 
+  /**
+  * Register the service provider.
+  *
+  * @return void
+  */
+  public function register()
+  {
+  //
+  }
 
-
-
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+   public function provides()
+   {
+     return [];
+   }
 
 }
