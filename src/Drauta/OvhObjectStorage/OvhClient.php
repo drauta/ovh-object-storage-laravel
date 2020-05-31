f7c565c129e2efd63d63f32cd77c906e1ad01245
mpuig9406 <mpuig9406@gmail.com>
2020-05-31 08:06:03 +0200

    Update OvhClient.php
    v3 api ovh

--- src/Drauta/OvhObjectStorage/OvhClient.php
+++ src/Drauta/OvhObjectStorage/OvhClient.php
@@ -1,12 +1,16 @@
 <?php namespace Drauta\OvhObjectStorage;
 
 /*Dependencias*/
-use OpenCloud\OpenStack;
+//use OpenCloud\OpenStack;
+use OpenStack\OpenStack;
 use Guzzle\Http\Exception\BadResponseException;
+use OpenStack\ObjectStore\v1\Models\Container;
+use Carbon\Carbon;
+use GuzzleHttp\Psr7\Stream;
 
 
 class OvhClient{
-	private $url = "https://auth.cloud.ovh.net/v2.0/";
+	private $url = "https://auth.cloud.ovh.net/v3.0/";
 	private $client;
 	private $region;
 	private $container;
@@ -15,30 +19,62 @@ class OvhClient{
 	
 	private function getContainer()
 	{
-	  if (!$this->container){
-	          $this->container = $this->client->objectStoreService('swift', $this->region, 'publicURL')->getContainer($this->container_name);		
-    	  }
-    
-          return $this->container;
+	    if (!$this->container){
+		   $this->container = $this->client->objectStoreV1()->getContainer('tsaplus');	
+        }
+        return $this->container;
 	}
-	
-	public function __construct($client)
-	{		
-	  $this->client = new OpenStack($this->url, array(
-	    'username' => $client['username'],
-	    'password' => $client['password'],	  
-	    'tenantId' => $client['tenantId'],
-	  ));
-	  $this->region = $client['region'];
-	  $this->container_name = $client['container'];
-	  $this->container_url = $client['container_url'];
-	}
+
+    public function __construct($client, $cache)
+    {	
+        $options = [
+        	'authUrl' => $client['authUrl'],
+        	'region' => $client['region'],
+        	'user' => [
+        		'name' => $client['username'],
+        		'password' => $client['password'],
+        		'domain' => [
+        			'id' => 'default',
+        		],
+        	],
+        	'scope' => [
+        		'project' => [
+        			'id' => $client['projectId'],
+        		],
+        	],
+        	'publicUrl' => $client['container_url'],
+        ];
+
+
+		$this->client = new OpenStack($options);
+
+		$identity = $this->client->identityV3(['region' => $client['region']]);
+
+
+        if ($token = $cache->get('openstack-token')) {
+			$options['cachedToken'] = $token;
+
+        } else {
+        	$token = $identity->generateToken(['user' => $options['user']]);
+
+        	$cache->put(
+        		'openstack-token',
+        		$token->export(),
+				//Carbon::now()->diffInSeconds(Carbon::createDateTimeImmutable($token->expires))
+				Carbon::now()->diffInSeconds(new Carbon($token->expires->format('Y-m-d H:i:s.u'), $token->expires->getTimezone()))
+        	);
+        
+        	$options['cachedToken'] = $token->export();
+		}
+
+        //$container = $this->client->objectStoreV1()->getContainer($client['containerName']);
+    }
 
 	public function fileGet($filename, $break_cache = false)
 	{
-          $url = $this->getContainer()->getObject($filename)->getUrl();
-  	  if ($break_cache) $url .= '?'.time();
-	    return $url;
+        $url = $this->getContainer()->getObject($filename)->getUrl();
+  	    if ($break_cache) $url .= '?'.time();
+	        return $url;
 	}
 	
 	public function fileGetUrl($filename, $break_cache = false)
@@ -67,8 +103,16 @@ class OvhClient{
 	    }else{
 	      $filename = $file->getClientOriginalName();
 	    }
-	  }	
-	  $this->getContainer()->uploadObject($filename, fopen($getPath, 'r'));				
+	  }		  
+	  //$this->getContainer()->upload($filename, fopen($getPath, 'r'));	
+
+	  // You can use any instance of \Psr\Http\Message\StreamInterface
+      $stream = new Stream(fopen($getPath, 'r'));
+	  $options = [
+		'name'    => $filename,
+		'stream' => $stream,
+	  ];
+	  $this->getContainer()->createObject($options);			
 	}
 	
 	public function fileExists($filename)
